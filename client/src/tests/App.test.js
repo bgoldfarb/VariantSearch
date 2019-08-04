51ae5a4e2dee1f724e38b54665ef00353ab87728
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2019-08-04 11:02:11 -0400

    adding in more tests

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -9,6 +9,10 @@ const Adapter = require('enzyme-adapter-react-16')
 
 
 Enzyme.configure({ adapter: new Adapter() })
+let wrapper;
+beforeEach(() => {
+wrapper = shallow(<App />);
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -16,12 +20,22 @@ it('renders without crashing', () => {
 });
 
 it('renders a app logo', () => {
-  const app = shallow(<App />);
-  expect(app.find('.App-logo')).to.have.lengthOf(1);
+  expect(wrapper.find('.App-logo')).to.have.lengthOf(1);
 });
 
 it('renders both children components', () => {
-  const wrapper = shallow(<App />);
   expect(wrapper.find(AutoComplete)).to.have.lengthOf(1);
   expect(wrapper.find(CreateTable)).to.have.lengthOf(1);
 });
+
+it('renders one class component with name App-Component', () => {
+  expect(wrapper.find('div.App-Component')).to.have.length(1)
+})
+
+it('renders text properly', () => {
+  expect(wrapper.find('h1.App-title').text()).to.be.equal("Variant Search")
+})
+
+it('returns all the children nodes', () => {
+    expect((wrapper.find('div.App').children())).to.have.lengthOf(4)
+})
---
+++ client/src/tests/CreateTable.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Enzyme, {shallow, mount} from 'enzyme'
+import chai, {expect} from 'chai'
+import sinonChai from 'sinon-chai'
+import { spy } from 'sinon'
+import CreateTable from '../components/CreateTable';
+import { wrap } from 'module';
+
+const Adapter = require('enzyme-adapter-react-16')
+chai.use(sinonChai)
+Enzyme.configure({ adapter: new Adapter() })
+let wrapper;
+
+const mockedData = {
+  gene : 'stuff',
+  nucChange: 'nucChange'
+}
+
+beforeEach(() => {
+  wrapper = shallow(<CreateTable {...mockedData}/>);
+})
+
+
+it('renders the CreateTable component', () => {
+  expect(wrapper).to.exist
+});
+
+it('should render all children nodes correctly on start', () => {
+  expect(wrapper.find('div.table-variants').children()).to.have.lengthOf(1)
+})
+
+it('should render the table header correctly with Capitalization', () => {
+  expect(wrapper.find('th').at(0).text()).to.equal('GENE')
+  expect(wrapper.find('th').children()).to.have.lengthOf(9)
+})
+
+it('should render the correct table data when passed in as props', () => {
+  expect(wrapper.find('td').at(0).text()).to.equal('stuff')
+  expect(wrapper.find('td').at(1).text()).to.equal('nucChange')
+
+})
